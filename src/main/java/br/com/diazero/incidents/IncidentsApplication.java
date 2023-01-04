1221a5d415f7c8232e99d153a26297951a5ee34a
Gabriel Queiroz <gabriel-cso@hotmail.com>
2023-01-04 20:36:10 -0300

    Incidents project created

---
+++ src/main/java/br/com/diazero/incidents/IncidentsApplication.java
@@ -0,0 +1,13 @@
+package br.com.diazero.incidents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IncidentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IncidentsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/diazero/incidents/IncidentsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.diazero.incidents;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IncidentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
